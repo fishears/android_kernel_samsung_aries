2a6bb7c8c024c8eb139ce7b1c7b16fdd34b99f94
fishears <f1she4rs@gmail.com>
2012-11-06 14:20:29 +0000

    kernel name and version added "0.10"

--- init/version.c
+++ init/version.c
@@ -44,5 +44,5 @@ const char linux_banner[] =
 
 const char linux_proc_banner[] =
 	"%s version %s"
-	" (" LINUX_COMPILE_BY "@" LINUX_COMPILE_HOST ")"
+	" (fishears_kernel_0.10)"
 	" (" LINUX_COMPILER ") %s\n";
