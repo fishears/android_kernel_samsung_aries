31bd85bdbd6c2d163f29697f657147be0cf1a03b
fishears <f1she4rs@gmail.com>
2013-01-21 08:20:35 +0000

    add 1300 and 1100 OC steps

--- arch/arm/mach-s5pv210/mach-aries.c
+++ arch/arm/mach-s5pv210/mach-aries.c
@@ -438,12 +438,19 @@ static struct s5p_media_device aries_media_devs[] = {
 #ifdef CONFIG_CPU_FREQ
 static struct s5pv210_cpufreq_voltage smdkc110_cpufreq_volt[] = {
 	{
-
 		.freq	= 1400000,
 		.varm	= 1350000,
 		.vint	= 1150000,
 	}, {
-		.freq	= 1000000,
+		.freq	= 1300000,
+		.varm	= 1325000,
+		.vint	= 1125000,
+	}, {
+		.freq	= 1200000,
+		.varm	= 1275000,
+		.vint	= 1100000,
+	}, {
+		.freq	= 1100000,
 		.varm	= 1275000,
 		.vint	= 1100000,
 	}, {
@@ -451,12 +458,10 @@ static struct s5pv210_cpufreq_voltage smdkc110_cpufreq_volt[] = {
 		.varm	= 1275000,
 		.vint	= 1100000,
 	}, {
-
 		.freq	=  800000,
 		.varm	= 1200000,
 		.vint	= 1100000,
 	}, {
-
 		.freq	=  400000,
 		.varm	= 1050000,
 		.vint	= 1100000,
@@ -4960,6 +4965,7 @@ static int wlan_carddetect_en(int onoff)
 	udelay(5);
 
 	sdhci_s3c_force_presence_change(&s3c_device_hsmmc1);
+	msleep(500); /* wait for carddetect */
 	return 0;
 }
 
